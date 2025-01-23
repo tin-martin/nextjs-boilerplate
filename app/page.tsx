e8b7ac274f506ffed3d5ef4de54c568f7121bf07
Martin Tin <75148357+tin-martin@users.noreply.github.com>
2025-01-22 19:05:27 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gradient-to-br from-darkPurple to-purple-800 text-offWhite">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gradient-to-br from-darkPurple to-teal text-offWhite">
       {/* Rotating Emoji */}
       <div
         className="text-6xl animate-spin-slow"
