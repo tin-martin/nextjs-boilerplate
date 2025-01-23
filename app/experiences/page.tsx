babf0c59063745211060b9b2042f32511b81bfe1
Martin Tin <75148357+tin-martin@users.noreply.github.com>
2025-01-22 19:38:14 -0500

    Update page.tsx

--- app/experiences/page.tsx
+++ app/experiences/page.tsx
@@ -5,27 +5,35 @@ export default function Experiences() {
       <p className="text-sm sm:text-base leading-relaxed mb-4">
         My journey into engineering design has been shaped by a combination of creative pursuits and technical interests:
       </p>
-                       <ol class="relative border-s border-gray-200 dark:border-gray-700">                  
-                      <li class="mb-10 ms-4">
-                          <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                          <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Ages 1–12</time>
-                          <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Creative Foundations</h3>
-                          <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">As a child, I immersed myself in creative activities such as playing the violin and drawing. These experiences nurtured my creative thinking and appreciation for aesthetics.</p>
-                      </li>
-                      <li class="mb-10 ms-4">
-                          <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                          <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Ages 13–16</time>
-                          <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Exploration of Coding</h3>
-                          <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Coding became a natural extension of my curiosity and desire to create. I began making fun low-level games which helped me gain an appreciation for the intersection of creativity and engineering.</p>
-                      </li>
-                      <li class="ms-4">
-                          <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                          <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Ages 16–Ongoing</time>
-                          <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Integrating Creativity into Engineering Design</h3>
-                          <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">I began to consciously recognize my desire to combine my love for creative expression with engineering concepts through code. With this, I undertook creative coding projects, visually expressing various computer science and mathematical concepts. My introduction to engineering design was primarily in UI and web design, which I found particularly fulfilling because it combines functionality with visual expression.</p>
-                          <p class="text-base font-normal text-gray-500 dark:text-gray-400">For example, in the aforementioned design report for a bubble tea cup, my creative background allowed me to explore unconventional solutions that went beyond the status quo.</p>
-                      </li>
-                  </ol>
+      <ol className="relative border-s border-gray-200 dark:border-gray-700">
+        <li className="mb-10 ms-4">
+          <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+          <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Ages 1–12</time>
+          <h3 className="text-lg font-semibold text-mint">Creative Foundations</h3>
+          <p className="mb-4 text-base font-normal text-gray-300 dark:text-gray-400">
+            As a child, I immersed myself in creative activities such as playing the violin and drawing. These experiences nurtured my creative thinking and appreciation for aesthetics.
+          </p>
+        </li>
+        <li className="mb-10 ms-4">
+          <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+          <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Ages 13–16</time>
+          <h3 className="text-lg font-semibold text-mint">Exploration of Coding</h3>
+          <p className="mb-4 text-base font-normal text-gray-300 dark:text-gray-400">
+            Coding became a natural extension of my curiosity and desire to create. I began making fun low-level games which helped me gain an appreciation for the intersection of creativity and engineering.
+          </p>
+        </li>
+        <li className="ms-4">
+          <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+          <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Ages 16–Ongoing</time>
+          <h3 className="text-lg font-semibold text-mint">Integrating Creativity into Engineering Design</h3>
+          <p className="mb-4 text-base font-normal text-gray-300 dark:text-gray-400">
+            I began to consciously recognize my desire to combine my love for creative expression with engineering concepts through code. With this, I undertook creative coding projects, visually expressing various computer science and mathematical concepts. My introduction to engineering design was primarily in UI and web design, which I found particularly fulfilling because it combines functionality with visual expression.
+          </p>
+          <p className="text-base font-normal text-gray-300 dark:text-gray-400">
+            For example, in the aforementioned design report for a bubble tea cup, my creative background allowed me to explore unconventional solutions that went beyond the status quo.
+          </p>
+        </li>
+      </ol>
       <p className="text-sm sm:text-base leading-relaxed mt-4">
         These experiences have taught me to view engineering design as an inherently creative discipline. While it is often seen as objective and structured, my background helps me approach design challenges with out-of-the-box thinking and innovative ideas. For example, in the aforementioned design report for a bubble tea cup, my creative background allowed me to explore unconventional solutions that went beyond the status quo.
       </p>
